ab863484e15b04c0035fc63fd3d2000260dff3a6
naman.bhardwaj <naman.bhardwaj@hexaview.com>
2024-10-25 13:35:03 +0530

    Added the following- 1. Saved unanswered questions in google sheets. 2. Handle audio messages

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,6 +1,11 @@
+import mimetypes
 import os
+import re
+import requests
 from fastapi import Request, APIRouter
 from fastapi.logger import logger
+from openai import OpenAI
+from requests import Timeout
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from src.services.ai_responses import get_ai_response
@@ -13,8 +18,15 @@ whatsapp = APIRouter(
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 twilio_number_code = os.environ.get("TWILIO_NUMBER_CODE")
+OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
 
-client = Client(account_sid, auth_token)
+
+twilio_client = Client(account_sid, auth_token)
+client = OpenAI(
+    api_key=OPEN_AI_API_KEY,
+)
+
 
 # Define a route for handling incoming WhatsApp messages
 @whatsapp.post('/whatsapp')
@@ -26,13 +38,59 @@ async def whatsapp_endpoint(request: Request):
     try:
         form = await request.form()
         user_message = form['Body'].lower()  # User's message
-        user_number = form['Author']  # User's phone number
-        logger.info(f"Received request with query - {user_message}")
-        ai_response = get_ai_response(user_phone=user_number, message=user_message)
+        user_number = form['From']
 
-        # Create Twilio WhatsApp response
-        client.conversations.v1.conversations(form['ConversationSid']).messages.create(body=ai_response)
+        media_url = form.get("MediaUrl0")  # For media (voice recordings)
+        media_content_type = form.get("MediaContentType0")  # Content type (e.g., audio/ogg)
+
+        if user_message:
+            logger.info(f"User message (Text) : {user_message}")
+            response = get_ai_response(user_phone=user_number, message=user_message)
+
+        elif media_url and media_content_type.startswith("audio/"):
+            # Handle voice message (requires transcription)
+            user_message = transcribe_audio(media_url)
+            if user_message:
+                logger.info(f"User message (Audio) : {user_message}")
+                response = get_ai_response(user_phone=user_number, message=user_message)
+            else:
+                response = "Could not transcribe voice message."
+
+        logger.info(f"Received request with query - {user_message}")
+
+        pattern = r'【\d+:\d+†[^\]]+】'
+        response = re.sub(pattern, '', response)
+        twilio_client.messages.create(
+            body=response,
+            from_=TWILIO_PHONE_NUMBER,
+            to=user_number
+        )
 
         return MessagingResponse()
     except Exception as e:
         logger.error(f"Got an exception - {e}")
+
+
+def transcribe_audio(media_url):
+    auth = (account_sid, auth_token)
+    try:
+        response = requests.get(media_url, auth=auth, timeout=10)
+        response.raise_for_status()
+    except Timeout:
+        return None
+    except Exception as e:
+        logger.exception(f"Error while transcribing audio {e}")
+
+    content_type = response.headers['Content-Type']
+    ext = mimetypes.guess_extension(content_type)
+    if ext not in ['.flac', '.m4a', '.mp3', '.mp4', '.mpeg', '.mpga', '.oga', '.ogg', '.wav', '.webm']:
+        return None
+
+    temp_audio_file = f"temp_audio{ext}"
+    with open(temp_audio_file, 'wb') as f:
+        f.write(response.content)
+
+    with open(temp_audio_file, 'rb') as f:
+        response = client.audio.transcriptions.create(model="whisper-1", file=f)
+        logger.info(f"Transcribed audio message as - {response.text}")
+        return response.text
--- src/services/ai_responses.py
+++ src/services/ai_responses.py
@@ -1,15 +1,21 @@
+import json
 import os
+from fastapi.logger import logger
 from openai import OpenAI
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 ORGANIZATION = os.environ.get("ORGANIZATION")
 PROJECT_ID = os.environ.get("PROJECT_ID")
 ASSISTANT_ID = os.environ.get("ASSISTANT_ID")
+GOOGLE_SERVICE_ACCOUNT_URL = os.environ.get("GOOGLE_SERVICE_ACCOUNT_URL")
+GOOGLE_SHEET_URL = os.environ.get("GOOGLE_SHEET_URL")
 
 user_thread_info = {}
 
 client = OpenAI(
-  api_key=OPENAI_API_KEY,
+  api_key=OPEN_AI_API_KEY,
   organization=ORGANIZATION,
   project=PROJECT_ID,
 )
@@ -20,7 +26,7 @@ def get_ai_response(user_phone, message):
     this function will generate the response for the given query
     """
     thread_id = add_message_in_thread(user_phone, message)
-    text = run_thread(thread_id)
+    text = run_thread(thread_id, user_phone)
     return text
 
 
@@ -42,7 +48,7 @@ def add_message_in_thread(user_phone, message):
     return thread_id
 
 
-def run_thread(thread_id):
+def run_thread(thread_id, user_phone):
     """
     this function will finally run the thread and return the generated response
     """
@@ -50,13 +56,64 @@ def run_thread(thread_id):
         thread_id=thread_id,
         assistant_id=ASSISTANT_ID
     )
+    tool_outputs = []
     while True:
         run = client.beta.threads.runs.retrieve(
             thread_id=thread_id,
             run_id=run.id
         )
+
         if run.status == "completed":
+            logger.info("Run completed")
             messages = client.beta.threads.messages.list(thread_id=thread_id)
             latest_message = messages.data[0]
             text = latest_message.content[0].text.value
             return text
+
+        elif run.status == 'requires_action':
+            logger.info("Run required action")
+            for function_call in run.required_action.submit_tool_outputs.tool_calls:
+                if function_call.function.name == "handle_unanswered_question":
+                    arguments = json.loads(function_call.function.arguments)
+                    unanswered_question = arguments.get("question")
+                    save_unanswered_questions_to_sheet(unanswered_question, user_phone)
+
+                    tool_outputs.append({
+                        "tool_call_id":
+                            function_call.id,
+                        "output":
+                            "Unanswered question noted and saved to Google Sheets."
+                    })
+
+            if tool_outputs:
+                try:
+                    client.beta.threads.runs.submit_tool_outputs_and_poll(
+                        thread_id=thread_id,
+                        run_id=run.id,
+                        tool_outputs=tool_outputs)
+                    print("Tool outputs submitted successfully.")
+                except Exception as e:
+                    print("Failed to submit tool outputs:", e)
+
+            text = "Sorry I have not been taught how to answer that question. " \
+                   "We will make a note of that question and I will be able to answer it soon. " \
+                   "Please try rephrasing the question and be specific so that I can understand the question better."
+            return text
+
+
+# Google Sheets setup and authorization
+def save_unanswered_questions_to_sheet(unanswered_question, user_phone):
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        GOOGLE_SERVICE_ACCOUNT_URL, scope)
+    client = gspread.authorize(creds)
+    # Open the specific Google Sheet (worksheet 1 assumed for unanswered questions)
+    sheet = client.open_by_url(GOOGLE_SHEET_URL)
+    sheet = sheet.sheet1
+    # Append each unanswered question to the sheet
+    user_phone = user_phone.split(":")[-1]
+    sheet.append_row([unanswered_question, user_phone])
+    logger.info("Unanswered questions saved to Google Sheets.")
